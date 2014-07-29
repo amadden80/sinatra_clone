089adec01b08c7e321a117d3528234d4c54bfe7e
mattmarcello <matt.marcello@gmail.com>
2014-07-29 14:29:28 -0400

    remove unnecessary comments and binding statements

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@
 # Q: What is the view still doing in app.rb?
 # A: It should not be there, let's move it lib.
 
+# Q: This feels very close to the metal.  How can we abstract away from rack
+# A: Applications should inherit from application class.
+
 require 'rack'
 require 'erb'
 require 'pry'
@@ -24,60 +27,39 @@ class Router
 
   def route(req)
     method, path = req.request_method.downcase.to_sym, req.path_info
-    binding.pry
-    @routes[method][path].call
+    @routes[method][path].call(req)
   end
+
 end
-
-
 
 class Application
 
   BEATLES = ["john", "paul", "george", "ringo", "yoko"]
   STONES  = ['mick', 'keith', 'ronnie', 'charlie']
 
+
   def initialize
     @router = Router.new
-    @router.create_route('get', '/beatles') do
+
+    # we have to pass the request into the block
+    @router.create_route('get', '/beatles') do |req|
       @band = BEATLES
       @name = "beatles"
+      [200, {"Content-Type" => "text/html"}, [View.new({view: 'band'}).render(binding)]]
+    end
+
+    @router.create_route('post', '/beatles') do |req|
+      @band = BEATLES
+      @name = "beatles"
+      BEATLES.push(req.params['member']) if req.params['member']
       [200, {"Content-Type" => "text/html"}, [View.new({view: 'band'}).render(binding)]]
     end
   end
 
   def call(env)
-    # case path = env["PATH_INFO"]
-    # As we start to develop more complex behaviors on the server,
-    # it is advantageous for us to create a new Rack::Request object
     req = Rack::Request.new(env)
     @router.route(req)
   end
-    # case req.path_info
-    # when /beatles/
-    #   @band = BEATLES
-    #   @name = "beatles"
-    #   if req.request_method == 'GET'
-    #     [200, {"Content-Type" => "text/html"}, [View.new({view: 'band'}).render(binding)]]
-    #   elsif req.request_method == 'POST'
-    #     member = req.params['member']
-    #     BEATLES.pus2h(member) if req.params['member']
-    #     [200, {"Content-Type" => "text/html"}, [View.new({view: 'band'}).render(binding)]]
-    #   end
-    # when /stones/
-    #   @band = STONES
-    #   @name = "stones"
-    #   if req.request_method == 'GET'
-    #     [200, {"Content-Type" => "text/html"}, [View.new({view: 'band'}).render(binding)]]
-    #   elsif
-    #     member = req.params['member']
-    #     BEATLES.push(member) if req.params['member']
-    #     [200, {"Content-Type" => "text/html"}, [View.new({view: 'band'}).render(binding)]]
-    #   end
-    # else
-    #   binding.pry
-    #   [404, {"Content-Type" => "text/html"}, ["<h1>404: NOT FOUND</h1>"]]
-    # end
-  # end
 end
 
 thin = Rack::Handler::Thin
--- lib/router.rb
+++ lib/router.rb
@@ -1,27 +1,2 @@
-class Router
-  attr_accessor :routes
-
-  def intitialize(routes = {})
-    @routes = routes
-  end
-
-
-
-
-end
-
-
-
-
-
-
-
-
-# get is a method on your application
-
-
-get '/beatles' do
-
-end
 
 
